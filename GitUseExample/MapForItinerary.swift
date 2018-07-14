6985fcabe4f42e5b512f8c1f2c14a84030f744b3
Alex <trashertz@gmail.com>
2018-07-14 03:22:02 +0200

    add new class map !

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Alex Trashertz on 14/07/18.
+//  Copyright © 2018 Alex Trashertz. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
