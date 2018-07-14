d4e8515d3c640a4b47dd806081a0a9ca97a576be
Alex <trashertz@gmail.com>
2018-07-14 04:24:47 +0200

    FINISH WORK

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("This is a Git tutorial")
+        print ( "! Hai finito" )
         // Do any additional setup after loading the view, typically from a nib.
     }
 
